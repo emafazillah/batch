7f7acfcea53e7d944329b29f1bba96eeb84fd69a
Ema Fazillah binti Abu Bakar <gatsu2811@gmail.com>
2019-02-19 02:13:49 +0800

    Add unit test

--- src/main/java/com/example/spring/batch/BatchApplicationVerTwo.java
+++ src/main/java/com/example/spring/batch/BatchApplicationVerTwo.java
@@ -5,28 +5,49 @@ public class BatchApplicationVerTwo {
 	private static int attemptCount = 1;
 	
 	private static int maxAttemptCount = 4;
-	
+		
 	public static void main(String...strings) {
 		while(attemptCount <= maxAttemptCount) {
-			try {
-//				if(attemptCount == 4) {
-//					new Exception("More than 3 attempts");
-//				} else {
-//					System.out.println("Run application");
-//				}
-				if(attemptCount < 4) {
-					throw new Exception("Deadlock. Retry...");
-				} else {
-					System.out.println("Run application");
-					break;
-				}
-			} catch(Exception ex) {
-				++attemptCount;
-				String errMsg = ex.getStackTrace().toString();
-				System.out.println("Error message: " + errMsg);
+			if(attemptCount < maxAttemptCount) {
+				Launch launch = new Launch();
+				attemptCount = launch.launch(attemptCount, strings);
+			} else {
+				System.out.println("Reached maximum attempt.");
+				break;
 			}
-			
 		}
 	}
+	
+}
 
+class Launch {
+
+	public Launch() {
+		
+	}
+	
+	public int launch(int attemptCount, String...strings) {
+		try {
+			// Test to simulate Exception without unit test
+//			if(attemptCount < 4) {
+//				throw new Exception("Deadlock. Retry...");
+//			} else {
+//				System.out.println("Run application");
+//				Thread.sleep(1000); // dummy sleep
+//				attemptCount = 5;
+//			}
+			
+			System.out.println("Run application");
+			Thread.sleep(1000); // dummy sleep
+			attemptCount = 5;
+			
+		} catch(Exception ex) {
+			++attemptCount;
+			String errMsg = ex.getStackTrace().toString();
+			System.out.println("Error message: " + errMsg);
+		}
+		
+		return attemptCount;
+	}
+	
 }
--- src/test/java/com/example/spring/batch/BatchApplicationTests.java
+++ src/test/java/com/example/spring/batch/BatchApplicationTests.java
@@ -1,17 +1,39 @@
 package com.example.spring.batch;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.junit.Test;
+import org.powermock.api.mockito.PowerMockito;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class BatchApplicationTests {
 
 	@Test
-	public void testMain() {
+	public void testPositiveMain() throws Exception {
+		String[] mockStrings = new String[] {"2019-02-18", "JOB NAME"};
 		
+		Launch mockLaunch = PowerMockito.mock(Launch.class);
+		
+		PowerMockito.when(mockLaunch.launch(1, mockStrings)).thenReturn(5);
+		
+		BatchApplicationVerTwo.main(mockStrings);
+	}
+	
+	@Test
+	public void testNegativeMain() throws Exception {
+		String[] mockStrings = new String[] {"2019-02-18", "JOB NAME"};
+		
+		Launch mockLaunch = PowerMockito.mock(Launch.class);
+		
+		PowerMockito.when(mockLaunch.launch(3, mockStrings)).thenReturn(4);
+		
+		BatchApplicationVerTwo.main(mockStrings);
+		
+		assertEquals(4, mockLaunch.launch(3, mockStrings));
 	}
 
 }
