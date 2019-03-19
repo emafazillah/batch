1c1371c75ad94306b4b8818da23084c113e4a728
Ema Fazillah binti Abu Bakar <gatsu2811@gmail.com>
2019-03-19 09:34:43 +0800

    Fix JobPameters

--- src/main/java/com/example/spring/batch/BatchApplicationWithMsSql.java
+++ src/main/java/com/example/spring/batch/BatchApplicationWithMsSql.java
@@ -8,6 +8,7 @@ import javax.sql.DataSource;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -133,7 +134,8 @@ public class BatchApplicationWithMsSql {
 		
 		JobLauncher jobLauncher = context.getBean(JobLauncher.class);
         Job job = context.getBean(Job.class);
-        jobLauncher.run(job, new JobParameters());
+        JobParameters jobParameter = new JobParametersBuilder().addString("jobid", "TestJob").toJobParameters();
+        jobLauncher.run(job, jobParameter);
 	}
 
 }
